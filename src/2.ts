e5032cfeed2d05c3dda650888bae5e9420487a54
Valerii <vd.alternative@gmail.com>
2024-02-25 18:32:04 +0200

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -18,7 +18,11 @@ class Employee {
   // Заповніть модифікатори доступу
 
 
-  constructor(public name: string, private department: string, protected salary: number) {
+  constructor(
+    public name: string, 
+    private department: string, 
+    protected salary: number
+    ) {
     this.name = name;
     this.department = department;
     this.salary = salary;
